c4826eb860b109db724c4e8c9a00a4f0f0cf8874
praveensaravanan26 <23csb23@karpagamtech.ac.in>
2025-06-25 09:47:33 +0530

    Add files via upload

---
+++ linked list reversal.go
@@ -0,0 +1,15 @@
+type ListNode struct {
+ Val int
+ Next *ListNode
+}
+func reverseList(head *ListNode) *ListNode {
+ var prev *ListNode
+ curr := head
+ for curr != nil {
+ next := curr.Next
+ curr.Next = prev
+ prev = curr
+ curr = next
+ }
+ return prev
+}
